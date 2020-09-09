3691920d83db78b8a665267ccda95a0fe31a1f03
Bharath Prabha <bharathprabhakaran28@gmail.com>
2020-09-09 15:33:48 +0530

    Basic operation of BST.

--- bst.cpp
+++ bst.cpp
@@ -1,9 +1,154 @@
 #include <iostream>
 using namespace std;
 
+struct node
+{
+    int data;
+    struct node *right, *left;
+};
+
+struct node *new_node(int data)
+{
+    struct node *temp = new node();
+    temp->data = data;
+    temp->left = nullptr;
+    temp->right = nullptr;
+    return temp;
+};
+
+int min_value(struct node *root)
+{
+    struct node *temp = root;
+    while (temp->left != nullptr)
+    {
+        temp = temp->left;
+    }
+    return temp->data;
+}
+
+bool find_key(struct node *root, int data)
+{
+    if (root == nullptr)
+        return false;
+    else if (root->data > data)
+        return find_key(root->left, data);
+    else if (root->data < data)
+        return find_key(root->right, data);
+    else if (root->data == data)
+        return true;
+}
+
+int max_value(struct node *root)
+{
+    struct node *temp = root;
+    while (temp->right != nullptr)
+    {
+        temp = temp->right;
+    }
+    return temp->data;
+}
+
+void inorder(struct node *root)
+{
+    if (root != nullptr)
+    {
+        inorder(root->left);
+        cout << root->data;
+        inorder(root->right);
+    }
+}
+
+void preorder(struct node *root)
+{
+    if (root != nullptr)
+    {
+        cout << root->data;
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(struct node *root)
+{
+    if (root != nullptr)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        cout << root->data;
+    }
+}
+
+struct node *insert(struct node *root, int data)
+{
+    if (root == nullptr)
+        return new_node(data);
+    if (root->data >= data)
+        root->left = insert(root->left, data);
+    else
+        root->right = insert(root->right, data);
+    return root;
+}
+
+struct node *delete_key(struct node *root, int data)
+{
+    if (root == NULL)
+        return root;
+    else if (root->data > data)
+        root->left = delete_key(root->left, data);
+    else if (root->data < data)
+        root->right = delete_key(root->right, data);
+    else
+    {
+        if (root->left == nullptr)
+        {
+            struct node *temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if (root->right == nullptr)
+        {
+            struct node *temp = root->left;
+            free(root);
+            return temp;
+        }
+        else
+        {
+            struct node *temp = new_node(min_value(root->right));
+
+            root->data = temp->data;
+
+            root->right = delete_key(root->right, temp->data);
+        }
+    }
+    return root;
+}
+
 int main()
 {
-    cout << "hello world";
-    cout << "hello workd";
+    struct node *root = NULL;
+    root = insert(root, 5);
+    root = insert(root, 7);
+    root = insert(root, 2);
+    root = insert(root, 4);
+    root = insert(root, 1);
+    root = insert(root, 9);
+    root = insert(root, 4);
+    root = insert(root, 3);
+    inorder(root);
+    cout << " ";
+    preorder(root);
+    cout << " ";
+    postorder(root);
+    cout << " ";
+    cout << min_value(root);
+    cout << " ";
+    cout << max_value(root);
+    cout << " ";
+    cout << find_key(root, 200);
+    cout << " ";
+    cout << find_key(root, 9);
+    cout << " ";
+    root = delete_key(root, 9);
+    inorder(root);
     return 0;
 }
